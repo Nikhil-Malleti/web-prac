c50a9f55110b8bd9ed03b226e850c05554e590d8
Nikhil <cs21m009@iittp.ac.in>
2024-04-12 01:30:56 +0530

    adding a login page

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-// Select the clickable element
-const clickableElement = document.getElementById('clickableElement');
-
-// Add click event listener
-clickableElement.addEventListener('click', function() {
-  clickableElement.classList.toggle('glow');
-});
